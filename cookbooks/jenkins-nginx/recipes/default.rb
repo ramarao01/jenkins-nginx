9d5869bc9be9e2ccb6b4330d45853e8295e5b538
nexii <kedarasettiramarao01@gmail.com>
2016-02-19 14:31:21 +0530

    Jenkins and Nginx Installation

---
+++ cookbooks/jenkins-nginx/recipes/default.rb
@@ -0,0 +1,38 @@
+execute 'key2' do
+command 'sudo wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -'
+end
+
+execute 'key3' do
+command "sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'"
+end
+
+execute 'key4' do
+command "sudo apt-get -f install"
+end
+
+execute 'key5' do
+command "sudo apt-get update" 
+end
+
+package "openjdk-7-jre" 
+package "openjdk-7-jdk"
+
+execute 'key' do
+command 'sudo dpkg --configure -a'
+end
+
+package "jenkins"
+
+service "jenkins" do
+  supports [:stop, :start, :restart]
+  action [:start, :enable]
+end
+
+package 'nginx' do
+action :install
+end
+service "nginx" do
+ supports status: true
+ action [:enable, :start]
+end
+
