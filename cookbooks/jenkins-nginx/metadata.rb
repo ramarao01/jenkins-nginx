78d31a31b2e28113d765dab1730fc81b9e40a028
nexii <kedarasettiramarao01@gmail.com>
2016-02-23 13:02:17 +0530

    Added attributes and files folder

--- cookbooks/jenkins-nginx/metadata.rb
+++ cookbooks/jenkins-nginx/metadata.rb
@@ -1,4 +1,4 @@
-name 'jenkins'
+name 'jenkins-nginx'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
--- cookbooks/jenkins-nginx/recipes/default.rb
+++ cookbooks/jenkins-nginx/recipes/default.rb
@@ -1,23 +1,17 @@
-execute 'key2' do
-command 'sudo wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -'
-end
-
-execute 'key3' do
-command "sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'"
-end
-
-execute 'key4' do
-command "sudo apt-get -f install"
-end
-
-execute 'key5' do
-command "sudo apt-get update" 
+bash 'exe' do
+code <<-EOH
+sudo wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get -f install
+sudo apt-get update 
+EOH
 end
 
 package "openjdk-7-jre" 
+
 package "openjdk-7-jdk"
 
-execute 'key' do
+execute 'dpkg' do
 command 'sudo dpkg --configure -a'
 end
 
@@ -25,37 +19,81 @@ package "jenkins"
 
 service "jenkins" do
   supports [:stop, :start, :restart]
-  action [:start, :enable]
+  action [:enable, :start]
 end
 
-package 'nginx' do
-action :install
-end
+package 'nginx'
+
 service "nginx" do
  supports status: true
  action [:enable, :start]
 end
 
-
-execute 'key6' do
-command 'cat /file2 >> /etc/default/jenkins'
+cookbook_file '/etc/nginx/sites-enabled/default' do
+source 'default'
+cookbook 'jenkins-nginx'
+manage_symlink_source true
+owner 'root'
+group 'root'
+mode '0755'
+action:create
 end
 
-execute 'key7' do
-command 'cat /file1 > /etc/nginx/sites-enabled/default'
-end  
+bash 'key' do
+code <<-EOH
+sudo chmod 0755 /etc
+echo 'JENKINS_ARGS="--webroot=/var/cache/jenkins/war --httpListenAddress=127.0.0.1 --httpPort=$HTTP_PORT -ajp13Port=$AJP_PORT"' >> /etc/default/jenkins
+EOH
+end
+ssl_keyfile = '/etc/nginx/cert.key'
+ssl_crtfile = '/etc/nginx/cert.crt'
+ssl_signing_conf = '/etc/nginx/cert-ssl.conf'
 
+#ssl_keyfile = "#{node['nginx']['dir']}.key"
+#ssl_crtfile = "#{node['nginx']['dir']}.crt"
+#ssl_signing_conf = "#{node['nginx']['dir']}-ssl.conf"
+unless File.exists?(ssl_keyfile) && File.exists?(ssl_crtfile) && File.exists?(ssl_signing_conf)
+  file ssl_keyfile do
+    owner "root"
+    group "root"
+    mode "0644"
+    content `/opt/chefdk/embedded/bin/openssl genrsa 2048`
+    not_if { File.exists?(ssl_keyfile) }
+  end
 
+  file ssl_signing_conf do
+    owner "root"
+    group "root"
+    mode "0644"
+    not_if { File.exists?(ssl_signing_conf) }
+    content <<-EOH
+  [ req ]
+  distinguished_name = req_distinguished_name
+  prompt = no
+  [ req_distinguished_name ]
+  C                      = #{node['nginx']['ssl_country_name']}
+  ST                     = #{node['nginx']['ssl_state_name']}
+  L                      = #{node['nginx']['ssl_locality_name']}
+  O                      = #{node['nginx']['ssl_company_name']}
+  OU                     = #{node['nginx']['ssl_organizational_unit_name']}
+  CN                     = #{node['nginx']['server_name']}
+  emailAddress           = #{node['nginx']['ssl_email_address']}
+  EOH
+  end
 
+  ruby_block "create crtfile" do
+    block do
+      r = Chef::Resource::File.new(ssl_crtfile, run_context)
+      r.owner "root"
+      r.group "root"
+      r.mode "0755"
+      r.content `/opt/chefdk/embedded/bin/openssl req -config '#{ssl_signing_conf}' -new -x509 -nodes -sha1 -days 3650 -key #{ssl_keyfile}`
+      r.not_if { File.exists?(ssl_crtfile) }
+      r.run_action(:create)
+    end
+  end
+end
 
-
-
-
-
-
-
-
-
-
-
+node.default['nginx']['ssl_certificate'] ||= ssl_crtfile
+node.default['nginx']['ssl_certificate_key'] ||= ssl_keyfile
 
