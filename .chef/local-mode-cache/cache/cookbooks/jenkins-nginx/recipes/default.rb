7ea522aa125ca27696615da712a5ee0714c9f27a
nexii <kedarasettiramarao01@gmail.com>
2016-02-29 19:08:49 +0530

    ADD sed command which will comment JENKINS_ARGS line

--- .chef/local-mode-cache/cache/cookbooks/jenkins-nginx/recipes/default.rb
+++ .chef/local-mode-cache/cache/cookbooks/jenkins-nginx/recipes/default.rb
@@ -1,8 +1,3 @@
-#Installing Jenkins and Nginx as reverse proxy with ssl certificate
-
-#1st command in bash block downloads the key from the http link and adds it to apt-key list
-#2nd command adds link(from which jenkins has to be downloaded) to apt sources.list
-#3rd command will redownload broken packages if any
 bash 'exe' do
 code <<-EOH
 sudo wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
@@ -47,31 +42,30 @@ end
 bash 'key' do
 code <<-EOH
 sudo chmod 0755 /etc
-sed -i 's/JENKINS_ARGS/#JENKINS_ARGS/' /etc/default/jenkins 
 echo 'JENKINS_ARGS="--webroot=/var/cache/jenkins/war --httpListenAddress=192.168.56.103 --httpPort=$HTTP_PORT -ajp13Port=$AJP_PORT"' >> /etc/default/jenkins
 EOH
 end
-keyfile = '/etc/nginx/cert.key'
-crtfile = '/etc/nginx/cert.crt'
-sslconfig = '/etc/nginx/sslcert.conf'
+ssl_keyfile = '/etc/nginx/cert.key'
+ssl_crtfile = '/etc/nginx/cert.crt'
+ssl_signing_conf = '/etc/nginx/cert-ssl.conf'
 
-#If any file doesn't exist it will create all the three files 
-
-
-unless File.exists?(keyfile) && File.exists?(crtfile) && File.exists?(sslconfig)
-  file keyfile do
+#ssl_keyfile = "#{node['nginx']['dir']}.key"
+#ssl_crtfile = "#{node['nginx']['dir']}.crt"
+#ssl_signing_conf = "#{node['nginx']['dir']}-ssl.conf"
+unless File.exists?(ssl_keyfile) && File.exists?(ssl_crtfile) && File.exists?(ssl_signing_conf)
+  file ssl_keyfile do
     owner "root"
     group "root"
     mode "0644"
     content `/opt/chefdk/embedded/bin/openssl genrsa 2048`
-    not_if { File.exists?(keyfile) }
+    not_if { File.exists?(ssl_keyfile) }
   end
 
-  file sslconf do
+  file ssl_signing_conf do
     owner "root"
     group "root"
     mode "0644"
-    not_if { File.exists?(sslconf) }
+    not_if { File.exists?(ssl_signing_conf) }
     content <<-EOH
   [ req ]
   distinguished_name = req_distinguished_name
@@ -89,17 +83,19 @@ unless File.exists?(keyfile) && File.exists?(crtfile) && File.exists?(sslconfig)
 
   ruby_block "create crtfile" do
     block do
-      r = Chef::Resource::File.new(crtfile, run_context)
+      r = Chef::Resource::File.new(ssl_crtfile, run_context)
       r.owner "root"
       r.group "root"
       r.mode "0755"
-      r.content `/opt/chefdk/embedded/bin/openssl req -config '#{sslconfig}' -new -x509 -nodes -sha1 -days 3650 -key #{keyfile}`
-      r.not_if { File.exists?(crtfile) }
+      r.content `/opt/chefdk/embedded/bin/openssl req -config '#{ssl_signing_conf}' -new -x509 -nodes -sha1 -days 3650 -key #{ssl_keyfile}`
+      r.not_if { File.exists?(ssl_crtfile) }
       r.run_action(:create)
     end
   end
 end
 
+#node.default['nginx']['ssl_certificate'] ||= ssl_crtfile
+#node.default['nginx']['ssl_certificate_key'] ||= ssl_keyfile
 
 service "nginx" do
  supports status: true
